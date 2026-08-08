fced7828c00766fb78895b50a3adc4734aac1c72
bravecoderemail-bit <bravecoder.email@gmail.com>
2026-08-08 16:11:02 +0530

    08-august

--- comfy-store/src/Components/Context/CartContext.jsx
+++ comfy-store/src/Components/Context/CartContext.jsx
@@ -5,18 +5,61 @@ const CartContext = createContext();
 export function CartProvider({ children }) {
   const [cart, setCart] = useState([]);
 
+  //   function addToCart(item) {
+  //     setCart((prev) => [
+  //       ...prev,
+  //       {
+  //         id: item.id,
+  //         title: item.title,
+  //         price: item.price,
+  //         image: item.image,
+  //         description: item.description,
+  //         cartId: crypto.randomUUID(),
+  //       },
+  //     ]);
+  //   }
+
   function addToCart(item) {
-    setCart((prev) => [
-      ...prev,
-      {
-        id: item.id,
-        title: item.title,
-        price: item.price,
-        image: item.image,
-        description: item.description,
-        cartId: crypto.randomUUID(),
-      },
-    ]);
+    setCart((prev) => {
+      const existing = prev.find((i) => i.id === item.id);
+
+      if (existing) {
+        return prev.map((i) =>
+          i.id === item.id ? { ...i, quantity: i.quantity + 1 } : i,
+        );
+      }
+
+      return [
+        ...prev,
+        {
+          ...item,
+          quantity: 1,
+          cartId: crypto.randomUUID(),
+        },
+      ];
+    });
+  }
+
+  function increaseQuantity(cartId) {
+    setCart((prev) =>
+      prev.map((item) =>
+        cartId === item.cartId
+          ? { ...item, quantity: item.quantity + 1 }
+          : item,
+      ),
+    );
+  }
+
+  function decreaseQuantity(cartId) {
+    setCart((prev) =>
+      prev
+        .map((item) =>
+          cartId === item.cartId
+            ? { ...item, quantity: item.quantity - 1 }
+            : item,
+        )
+        .filter((item) => item.quantity > 0),
+    );
   }
 
   const removeFromCart = (cartId) => {
@@ -24,7 +67,15 @@ export function CartProvider({ children }) {
   };
 
   return (
-    <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+    <CartContext.Provider
+      value={{
+        cart,
+        addToCart,
+        removeFromCart,
+        increaseQuantity,
+        decreaseQuantity,
+      }}
+    >
       {children}
     </CartContext.Provider>
   );
